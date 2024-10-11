5082df83645c64c1c93c756bac03b2666ecb2523
Ashan Deen <ash474d@gmail.com>
2024-10-11 13:40:02 -0400

    update5

--- api/extract_terms.py
+++ api/extract_terms.py
@@ -13,7 +13,7 @@ CORS(app)
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=GROQ_API_KEY)
 
-@app.route('/api/extract_terms')#, methods=['POST']
+@app.route('/api/extract_terms', methods=['POST'])
 def extract_terms():
     data = request.json
     text = data.get('text')
@@ -27,5 +27,5 @@ def extract_terms():
     )
 
     result = response.choices[0].message.content
-    #return jsonify({"terms_and_definitions": result})
-    return "hi"
+    return jsonify({"terms_and_definitions": result})
+
